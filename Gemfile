df6e43eb457478855d69668c7b312e6460638180
Zia123456 <m.ziabakhteyari12@gmail.com>
2023-03-22 13:52:14 -0700

    Add authorization

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem "pg", "~> 1.1"
 
 gem 'devise'
 
+gem 'cancancan', '~> 1.9'
+
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
   
+    # Catch all CanCan errors and alert the user of the exception
+    rescue_from CanCan::AccessDenied do | exception |
+      redirect_to root_url, alert: exception.message
+    end
+
+
     protected
   
     def configure_permitted_parameters
